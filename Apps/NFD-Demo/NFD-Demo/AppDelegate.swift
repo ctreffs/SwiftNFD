006d565e8a4978368bcddab6e6e493c0fd89f16d
Christian Treffs <ctreffs@gmail.com>
2021-11-05 10:08:06 +0100

    Refine demo app

--- Apps/NFD-Demo/NFD-Demo/AppDelegate.swift
+++ Apps/NFD-Demo/NFD-Demo/AppDelegate.swift
@@ -12,10 +12,19 @@ import NFD
 class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet var window: NSWindow!
 
-    @IBAction func openDialog(_: NSButton) { print(NFD.OpenDialog()) }
-    @IBAction func openDialogMultiple(_: NSButton) { print(NFD.OpenDialogMultiple()) }
-    @IBAction func openSaveDialog(_: NSButton) { print(NFD.SaveDialog()) }
+    @IBAction func openDialog(_: NSButton) { print(NFD.OpenDialog(filter: getFileExtFilters)) }
+    @IBAction func openDialogMultiple(_: NSButton) { print(NFD.OpenDialogMultiple(filter: getFileExtFilters)) }
+    @IBAction func openSaveDialog(_: NSButton) { print(NFD.SaveDialog(filter: getFileExtFilters)) }
     @IBAction func openPickFolder(_: NSButton) { print(NFD.PickFolder()) }
+    @IBOutlet var fileExtFilters: NSTextFieldCell!
+
+    var getFileExtFilters: [String]? {
+        let string = fileExtFilters.stringValue
+        if string.isEmpty {
+            return nil
+        }
+        return string.components(separatedBy: ";")
+    }
 
     func applicationDidFinishLaunching(_: Notification) {
         // Insert code here to initialize your application
